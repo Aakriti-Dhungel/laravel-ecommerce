9861cd84a75eaea6b72d40060be5617f58c851ef
Aakriti-Dhungel <aakritidhungel01@gmail.com>
2025-10-07 12:34:38 +0545

    feat: enhance frontend layout and styling; integrate company data and improve navigation

--- resources/views/components/frontend-layout.blade.php
+++ resources/views/components/frontend-layout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="{{asset('./frontend/style.css')}}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('frontend/style.css')}}">
 
 </head>
 
@@ -17,9 +17,9 @@
     <main>
         {{ $slot  }}
     </main>
-    <footer></footer>
+    <footer>
+    </footer>
     
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 
 </body>
 
--- resources/views/components/frontend-navbar.blade.php
+++ resources/views/components/frontend-navbar.blade.php
@@ -1,13 +1,35 @@
-<nav>
-
-   <div class="container">
-      <div>
-         <img src="" alt="">
+<nav class="bg-primary">
+   <div class="container py-2">
+      <div class="flex items-center justify-between gap-2">
+         <a href="">
+            <img class="w-[100px] sm:w-[150px] md:w-[180px] lg:w-[220px]"
+               src="{{ asset(Storage::url($company->logo)) }}" alt="{{ $company->name }}">
+         </a>
+         <div class="hidden md:block">
+            <form action="" method="get">
+               <div class="flex items-center">
+                  <input type="text" name="q" id="q" placeholder="name">
+                  <button class="bg-gray-200 primary font-bold h-full px-4 py-2">Compare</button>
+               </div>
+            </form>
+         </div>
+         <div class="flex gap-4 items-center">
+            <a href="{{ route('login') }}" class="bg-secondary primary px-3 py-1 rounded">SignIn</a>
+            <a href="{{ route('register') }}"
+               class="border border-[var(--secondary)] secondary px-3 py-1 rounded">SIgnUp</a>
+         </div>
       </div>
-      <div></div>
-      <div></div>
 
+      <div class="md:hidden flex justify-center pt-4">
+         <form action="" method="get">
+            <div class="flex items-center">
+               <input type="text" name="q" id="q" placeholder="name">
+               <button class="bg-gray-200 primary font-bold h-full px-4 py-2">Compare</button>
+            </div>
+         </form>
+      </div>
    </div>
 
 
+   
 </nav>
